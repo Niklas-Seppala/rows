9dd12fbb028fe4ef6be721a258581df1fca99b1e
Niklas Seppala <niklas.t.seppala@gmail.com>
2024-08-17 17:01:41 +0300

    dunno

--- src/main/java/org/ns/TemperatureProcessor.java
+++ src/main/java/org/ns/TemperatureProcessor.java
@@ -1,5 +1,8 @@
 package org.ns;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.*;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
@@ -14,15 +17,61 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
-public class Main {
-    static final byte NEWLINE = 10;
-    static final byte CITY_DELIM = 59;
-    static final int MAP_SIZE = 256;
-    static final int BUFFER_SIZE = 256;
+public class TemperatureProcessor {
 
-    private static final int FIRST = 0;
+    public static void main(String[] args) throws Exception {
+        processFile("rows-tiny.data");
+    }
 
-    static class Temperature {
+    public static void processFile(@NotNull String file) throws Exception {
+        final int threads = Runtime.getRuntime().availableProcessors();
+        BlockingQueue<Map<String, Temperature>> resQueue = new ArrayBlockingQueue<>(threads);
+
+        try (FileChannel channel = FileChannel.open(Path.of(file),
+                StandardOpenOption.READ)) {
+            Thread[] runners = new Thread[threads];
+
+            final MemorySegment mmap = channel.map(FileChannel.MapMode.READ_ONLY,
+                    0,
+                    channel.size(),
+                    Arena.global());
+            long segment = 0;
+            long segmentSize = channel.size() / threads;
+            Runner prevRunner = null;
+            for (int i = 0; i < threads; i++) {
+                Runner runner = new Runner(segment, segment + segmentSize, resQueue, mmap, prevRunner);
+                runners[i] = new Thread(runner);
+                runners[i].start();
+                segment += segmentSize;
+                prevRunner = runner;
+            }
+
+            Map<String, Temperature> finalResult = new ConcurrentHashMap<>(Runner.MAP_SIZE);
+            for (int i = 0; i < threads; i++) {
+                Map<String, Temperature> result =  resQueue.poll(120, TimeUnit.SECONDS);
+                assert result != null;
+                result.entrySet().stream().parallel()
+                        .forEach((entry) -> finalResult.merge(entry.getKey(), entry.getValue(), Temperature::merge));
+            }
+            writeResultsTo(System.out, finalResult);
+        }
+    }
+
+    private static void writeResultsTo(final PrintStream out,
+                                       final Map<String, Temperature> results) {
+        out.print('{');
+        results.forEach((city, data) -> {
+            data.printToOut(out, city);
+            out.print(',');
+            out.print(' ');
+        });
+        out.print('\b');
+        out.print('\b');
+        out.print('}');
+        out.print('\n');
+    }
+
+    private static class Temperature {
         float high; double mean; float low; double sum; int count;
 
         public Temperature(float initial) {
@@ -30,7 +79,7 @@ public class Main {
             this.count = 1;
         }
 
-        public Temperature consume(Temperature other) {
+        public Temperature merge(Temperature other) {
             if (other.high > this.high) this.high = other.high;
             if (other.low < this.low) this.low = other.low;
             this.sum += other.sum;
@@ -39,7 +88,7 @@ public class Main {
             return this;
         }
 
-        public Temperature consume(float temperature) {
+        public Temperature merge(float temperature) {
             if (temperature > this.high) this.high = temperature;
             if (temperature < this.low) this.low = temperature;
             this.sum += temperature;
@@ -47,51 +96,50 @@ public class Main {
             return this;
         }
 
-        @Override
-        public String toString() {
-            return "%.2f/%.2f/%.2f".formatted(high, mean, low);
+        private void printToOut(final PrintStream out, String key) {
+            out.printf("%s=%.1f/%.1f/%.1f", key, high, mean, low);
         }
     }
 
-    static class Runner implements Runnable {
+    private static class Runner implements Runnable {
+        static final int BUFFER_SIZE = 256;
+        static final byte NEWLINE = 10;
+        static final byte CITY_DELIM = 59;
+        static final int MAP_SIZE = 1 << 14;
         private final long start;
         private long end;
-        public synchronized void setRewind(int offset) {
-            this.end -= offset;
-        }
+        @NotNull
         private final BlockingQueue<Map<String, Temperature>> resultQueue;
+        @NotNull
         private final MemorySegment mmap;
-        private final byte[] buffer = new byte[BUFFER_SIZE];
         private final HashMap<String, Temperature> map = new HashMap<>(MAP_SIZE);
+        @Nullable
         private final Runner previousRunner;
-        private final int order;
+        private final byte[] buffer = new byte[BUFFER_SIZE];
 
-        Runner(long start, long end, BlockingQueue<Map<String, Temperature>> resultQueue,
-               MemorySegment mmap, Runner previousRunner, int order) {
+        Runner(long start, long end,
+               @NotNull BlockingQueue<Map<String, Temperature>> resultQueue,
+               @NotNull MemorySegment mmap,
+               @Nullable Runner previousRunner) {
             this.start = start;
             this.end = end;
             this.resultQueue = resultQueue;
             this.mmap = mmap;
             this.previousRunner = previousRunner;
-            this.order = order;
         }
 
+
         /**
-         * <pre>
-         *  Halifax;-2.
-         *  Upington;20.
-         *  Toliara;22.
-         *  Edinburgh;1.
-         *  </pre>
          * @param length
          */
-        private void parse(int length, int count) {
+        private void parse(int length) {
             String city = null;
             float temperature = Integer.MIN_VALUE;
             for (int cursor = 0; cursor < length; cursor++) {
                 byte c = buffer[cursor];
                 if (c == CITY_DELIM) {
                     city = new String(buffer, 0, cursor);
+
                     temperature = Float.parseFloat(new String(buffer, cursor+1,
                             length - cursor-1));
                     break;
@@ -103,14 +151,18 @@ public class Main {
             final float temp = temperature;
             map.compute(city, (k, v) -> {
                 if (v == null) return new Temperature(temp);
-                return v.consume(temp);
+                return v.merge(temp);
             });
+        }
+
+        public synchronized void setRewind(int offset) {
+            this.end -= offset;
         }
 
         @Override
         public void run() {
             long cursor = start;
-            if (this.order != FIRST) { // TODO: Just check if previousRunner is null
+            if (this.previousRunner != null) {
                 // Seek the real start position. File mapping is partitioned by bytes,
                 // not by lines:
                 //      1. Rewind until newline is found
@@ -125,14 +177,13 @@ public class Main {
                 previousRunner.setRewind(rewindOffset);
             }
             int bufferCursor = 0;
-            int count = 0;
             while (cursor < end) {
                 byte c = mmap.get(ValueLayout.JAVA_BYTE, cursor++);
                 if (c != NEWLINE) {
                     assert bufferCursor < BUFFER_SIZE : "read-buffer overflow";
                     buffer[bufferCursor++] = c;
                 } else {
-                    parse(bufferCursor, ++count);
+                    parse(bufferCursor);
                     bufferCursor = 0;
                 }
             }
@@ -143,66 +194,5 @@ public class Main {
             }
         }
     }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        final int threads = Runtime.getRuntime().availableProcessors();
-        BlockingQueue<Map<String, Temperature>> resQueue = new ArrayBlockingQueue<>(threads);
-
-        try (FileChannel channel = FileChannel.open(Path.of("rows-small.data"),
-                StandardOpenOption.READ)) {
-            Thread[] runners = new Thread[threads];
-
-            final MemorySegment mmap = channel.map(FileChannel.MapMode.READ_ONLY,
-                    0,
-                    channel.size(),
-                    Arena.global());
-            long segment = 0;
-            long segmentSize = channel.size() / threads;
-            Runner prevRunner = null;
-            for (int i = 0; i < threads; i++) {
-                Runner runner = new Runner(segment, segment + segmentSize, resQueue, mmap, prevRunner, i);
-                runners[i] = new Thread(runner);
-                runners[i].start();
-                segment += segmentSize;
-                prevRunner = runner;
-            }
-
-            ConcurrentHashMap<String, Temperature> finalResult = new ConcurrentHashMap<>(MAP_SIZE);
-            for (int i = 0; i < threads; i++) {
-                Map<String, Temperature> result =  resQueue.poll(120, TimeUnit.SECONDS);
-                assert result != null;
-                result.entrySet().stream().parallel().forEach((e) -> finalResult.merge(e.getKey(), e.getValue(),
-                        Temperature::consume));
-            }
-            finalResult.forEach((city, temperature) -> System.out.println(city + "=" + temperature));
-        }
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
